2dde823abbbb3989866aa05f49f98883d41e7f17
mark-mansfield <markgmansfield@gmail.com>
2019-04-13 12:42:19 +1000

    basic drawer working

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,38 @@
 import React from "react"
+import PropTypes from "prop-types"
+import Button from "@material-ui/core/Button"
+/* eslint-disable import/first */
+import { withStyles } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import MenuIcon from "@material-ui/icons/Menu"
 import Footer from "../footer/footer"
+
+const drawStyles = {
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+}
 
 class Template extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       loading: "is-loading",
+      left: false,
     }
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    })
   }
 
   componentDidMount() {
@@ -23,9 +49,59 @@ class Template extends React.Component {
 
   render() {
     const { children } = this.props
+    // drawer set up
+    const { classes } = this.props
 
+    const sideList = (
+      <div className={classes.list}>
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
     return (
       <div className={`body ${this.state.loading}`}>
+        <div
+          className="hamburger_nav"
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            zIndex: "1001",
+          }}
+        >
+          <MenuIcon
+            className="menu-icon"
+            onClick={this.toggleDrawer("left", true)}
+          >
+            Open Left
+          </MenuIcon>
+        </div>
+        <Drawer
+          open={this.state.left}
+          onClose={this.toggleDrawer("left", false)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer("left", false)}
+            onKeyDown={this.toggleDrawer("left", false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
         <div id="wrapper">
           {children}
           <Footer />
@@ -34,5 +110,7 @@ class Template extends React.Component {
     )
   }
 }
-
-export default Template
+Template.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+export default withStyles(drawStyles)(Template)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
+
 import Waypoint from "react-waypoint"
 import Modal from "react-modal"
 import Layout from "../components/layout/layout"
@@ -16,12 +17,13 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from "react-accessible-accordion"
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect"
+// import {
+//   BrowserView,
+//   MobileView,
+//   isBrowser,
+//   isMobile,
+// } from "react-device-detect"
+
 import "../styles/accordion.css"
 
 // https://react-icons.netlify.com/#/icons/md
@@ -94,6 +96,7 @@ class Index extends React.Component {
         <Helmet title="Brown Sugar - dev site" />
 
         <Header modalState={this._handleShowModal.bind(this)} />
+
         <Modal
           closeTimeoutMS={300}
           style={style}
@@ -129,6 +132,7 @@ class Index extends React.Component {
           {/* because this component uses cssTransition group
           pass in a reference to the modal state so we can launch a modal
           from within this component */}
+
           <section id="menu__navigation">
             <Food modalState={this._handleShowModal.bind(this)} />
           </section>
@@ -176,108 +180,101 @@ class Index extends React.Component {
               <div />
             </div>
             <h3>Here is what you can choose from </h3>
-            <BrowserView>
-              <Accordion>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>ENTREE</AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <p>
-                      Roasted cauliflower, shawarma spices, almonds, currants,
-                      sage brown butter
-                    </p>
-                    <p>
-                      Seared scallops, caviar lentils, speck, tarragon and
-                      lettuce Roasted figs,
-                    </p>
-                    <p>
-                      stracciatella, pistachio, bulgur, balsamic South
-                      Australian mussels, chilli, tomato, chargrilled sourdough
-                    </p>
-                    <p>
-                      Spanish mackerel tataki, corn, cherry tomato, nori crisp,
-                      anchoïade
-                    </p>
-                    <p>
-                      Crispy pork belly, boudin noir, parsnip, apple, celery,
-                      jus
-                    </p>
-                    <p>
-                      Hand cut steak tartare, wild mushrooms, yolk, potato
-                      gaufrette
-                    </p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>MAIN</AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <p>
-                      Gold band Snapper, eggplant, roasted capsicum, okra, basil
-                      caper salsa
-                    </p>
-                    <p>
-                      Charred eggplant, crispy furikake rice, green tea dashi,
-                      shimeji
-                    </p>
-                    <p>
-                      Squid ink pasta, spanner crab, semi-dried tomato, chilli,
-                      saffron
-                    </p>
-                    <p> Fish pie, leeks, truffle oil</p>
-                    <p>
-                      Lamb rump, pistachio salsa verde, chargrilled onion,
-                      pumpkin, jus
-                    </p>
-                    <p>
-                      Chargrilled flat iron steak, parsnip, watercress, mushroom
-                    </p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>SIDES</AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <p>Pomme puree, crisp herb potatoes 9.5 </p>
-                    <p>Wild greens, chilli, confit garlic, pepitas 12 </p>
-                    <p>
-                      Beetroot, radicchio, pear &amp; walnuts, merlot
-                      vinaigrette 12
-                    </p>
-                    <p>Shaved cabbage, red apple, hazelnut, chives 12 </p>
-                    <p>
-                      Roasted carrots, baby onions, pistachio,sunflower sprouts
-                      12
-                    </p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-                <AccordionItem>
-                  <AccordionItemHeading>
-                    <AccordionItemButton>DESERT</AccordionItemButton>
-                  </AccordionItemHeading>
-                  <AccordionItemPanel>
-                    <p>
-                      Kaffir lime meringue, pineapple ginger sorbet, passion
-                      fruit, Thai basil Pain d’epice
-                    </p>
-                    <p>creme brûlée, rhubarb, strawberry sorbet</p>
-                    <p>
-                      Hazelnut chocolate fondant, salted butterscotch &amp;
-                      vanilla gelato
-                    </p>
-                    <p>
-                      Affogato, vanilla gelato, frangelico Sorbet or gelato{" "}
-                    </p>
-                    <p>Cheese plate, brown sugar oatcakes, fresh apple</p>
-                  </AccordionItemPanel>
-                </AccordionItem>
-              </Accordion>
-            </BrowserView>
 
-            <MobileView>Mobile view custom menu content</MobileView>
+            <Accordion>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>ENTREE</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>
+                    Roasted cauliflower, shawarma spices, almonds, currants,
+                    sage brown butter
+                  </p>
+                  <p>
+                    Seared scallops, caviar lentils, speck, tarragon and lettuce
+                    Roasted figs,
+                  </p>
+                  <p>
+                    stracciatella, pistachio, bulgur, balsamic South Australian
+                    mussels, chilli, tomato, chargrilled sourdough
+                  </p>
+                  <p>
+                    Spanish mackerel tataki, corn, cherry tomato, nori crisp,
+                    anchoïade
+                  </p>
+                  <p>
+                    Crispy pork belly, boudin noir, parsnip, apple, celery, jus
+                  </p>
+                  <p>
+                    Hand cut steak tartare, wild mushrooms, yolk, potato
+                    gaufrette
+                  </p>
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>MAIN</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>
+                    Gold band Snapper, eggplant, roasted capsicum, okra, basil
+                    caper salsa
+                  </p>
+                  <p>
+                    Charred eggplant, crispy furikake rice, green tea dashi,
+                    shimeji
+                  </p>
+                  <p>
+                    Squid ink pasta, spanner crab, semi-dried tomato, chilli,
+                    saffron
+                  </p>
+                  <p> Fish pie, leeks, truffle oil</p>
+                  <p>
+                    Lamb rump, pistachio salsa verde, chargrilled onion,
+                    pumpkin, jus
+                  </p>
+                  <p>
+                    Chargrilled flat iron steak, parsnip, watercress, mushroom
+                  </p>
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>SIDES</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Pomme puree, crisp herb potatoes 9.5 </p>
+                  <p>Wild greens, chilli, confit garlic, pepitas 12 </p>
+                  <p>
+                    Beetroot, radicchio, pear &amp; walnuts, merlot vinaigrette
+                    12
+                  </p>
+                  <p>Shaved cabbage, red apple, hazelnut, chives 12 </p>
+                  <p>
+                    Roasted carrots, baby onions, pistachio,sunflower sprouts 12
+                  </p>
+                </AccordionItemPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionItemHeading>
+                  <AccordionItemButton>DESERT</AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>
+                    Kaffir lime meringue, pineapple ginger sorbet, passion
+                    fruit, Thai basil Pain d’epice
+                  </p>
+                  <p>creme brûlée, rhubarb, strawberry sorbet</p>
+                  <p>
+                    Hazelnut chocolate fondant, salted butterscotch &amp;
+                    vanilla gelato
+                  </p>
+                  <p>Affogato, vanilla gelato, frangelico Sorbet or gelato </p>
+                  <p>Cheese plate, brown sugar oatcakes, fresh apple</p>
+                </AccordionItemPanel>
+              </AccordionItem>
+            </Accordion>
 
             <h3>Beverages </h3>
             <div style={{ width: "50%" }}>
@@ -355,6 +352,7 @@ class Index extends React.Component {
                 and Sundays from midday. Please don’t hesitate to give us a call
                 and one of our staff will be more than happy to help you.
               </p>
+              ˝
             </div>
           </section>
 
