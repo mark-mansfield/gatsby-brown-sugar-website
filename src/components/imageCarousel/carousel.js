f7184019ca5f7233ab3fab7826952c0e984850ab
mark-mansfield <markgmansfield@gmail.com>
2019-07-24 11:58:14 +1000

    updated deploy script and gatsy.config pathPrefix

--- src/components/imageCarousel/carousel.js
+++ src/components/imageCarousel/carousel.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Flickity from "react-flickity-component"
-import "../../styles/flickity.css"
+// import Flickity from "react-flickity-component"
+// import "../../styles/flickity.css"
 class imageCarousel extends React.Component {
   constructor(props) {
     super(props)
@@ -23,7 +23,7 @@ class imageCarousel extends React.Component {
 
     return (
       <div className="image-carousel">
-        {this.state.showCarousel && (
+        {/* {this.state.showCarousel && (
           <Flickity options={flickityOptions}>
             <img src="https://s3-ap-southeast-2.amazonaws.com/production.assets.merivale.com.au/wp-content/uploads/2018/11/22161742/Tottis-restaurant-Sydney-AB5I3905.jpg" />
             <img src="https://s3-ap-southeast-2.amazonaws.com/production.assets.merivale.com.au/wp-content/uploads/2018/11/22161748/Tottis-restaurant-Sydney-AB5I4332.jpg" />
@@ -32,7 +32,7 @@ class imageCarousel extends React.Component {
             <img src="https://s3-ap-southeast-2.amazonaws.com/production.assets.merivale.com.au/wp-content/uploads/2018/11/22161744/Tottis-restaurant-Sydney-AB5I4023.jpg" />
             <img src="https://s3-ap-southeast-2.amazonaws.com/production.assets.merivale.com.au/wp-content/uploads/2018/11/22161741/Tottis-restaurant-Sydney-AB5I3783.jpg" />
           </Flickity>
-        )}
+        )} */}
       </div>
     )
   }
